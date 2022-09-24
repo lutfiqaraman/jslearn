fac2bf90f9e5afadcf83b2d5e6eb52a09d4942fd
lutfiqaraman <lutfiqaraman@gmail.com>
2022-09-24 14:39:57 +0300

    use destructuring method

--- destructuring.js
+++ destructuring.js
@@ -8,3 +8,10 @@ const { name, room, weapon } =
 console.log("name: " + name.toLocaleLowerCase());
 console.log("kitchen: " + room.toLocaleLowerCase());
 console.log("weapon: " + weapon.toLocaleLowerCase());
+
+let a = 1;
+let b = 2;
+
+[b, a] = [a , b];
+console.log('a = ' + a);
+console.log('b = ' + b);
