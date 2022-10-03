abdd0c2723fd549682d4561c887975d9d730178f
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-03 20:13:41 +0300

    use map of underscoreJS library

--- map.js
+++ map.js
@@ -1,12 +1,9 @@
 let _ = require('underscore');
 let listNumber = [1, 2, 3];
-let listResult = [];
 
-_.map(listNumber, function (number) {
-    let result = number * 10;
-    listResult.push(result);
+let result = _.map(listNumber, function (number) {
+    return number * 10;
 });
 
 console.log(listNumber);
-console.log(listResult);
-
+console.log(result);
