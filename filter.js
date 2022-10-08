1b30c83ee6bb68ab0b0b6991b5c7d557455c7343
lutfiqaraman <lutfiqaraman@gmail.com>
2022-10-08 11:05:25 +0300

    use filter

--- filter.js
+++ filter.js
@@ -75,9 +75,34 @@ const videoData = [
     }
 ];
 
-let result = _.filter(videoData, function (data) {
-    return !!data.present;
+// Using UnderScore JS :: Start
+
+// Return only suspects that has present true
+const presentSuspects = _.filter(videoData, function (suspect) {
+    return suspect.present;
 });
 
-console.log(result);
+// Return only the name of the suspects
+const suspectsName = _.map(presentSuspects, function (suspect) {
+    return suspect.name;
+});
 
+console.log('The list of suspects that present that day are : ' + suspectsName);
+
+// Using UnderScore JS :: End
+
+// Using JS :: Start
+
+// Return only suspects that has present true
+const presentSuspectsJS = videoData.filter(function (suspect) {
+    return suspect.present;
+});
+
+// Return only the name of the suspects
+const suspectsNameJS = presentSuspectsJS.map(function (suspect) {
+    return suspect.name;
+});
+
+console.log('The list of suspects that present that day are : ' + suspectsNameJS);
+
+// Using JS :: End
